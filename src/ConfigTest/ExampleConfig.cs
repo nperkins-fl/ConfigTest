8134654df4a7704ad1d8815e00e867bd5b7846c4
Nick Perkins <nperkins@FrontlineEd.com>
2022-05-13 15:29:08 -0400

    added example of using a dictionary

--- src/ConfigTest/ExampleConfig.cs
+++ src/ConfigTest/ExampleConfig.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ConfigTest;
 
 public class ExampleConfig
@@ -6,4 +8,11 @@ public class ExampleConfig
     public string Setting2 { get; set; }
     public string Setting3 { get; set; }
     public string[] Array { get; set; }
+    public Dictionary<string, DictionaryItem> Dictionary { get; set; }
+}
+
+public class DictionaryItem
+{
+    public string Property1 { get; set; }
+    public string Property2 { get; set; }
 }
